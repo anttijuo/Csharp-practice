ba02a9b5f1902be0ae5dba032dd514ad89d4c209
Antti Juotasniemi <anttijuo@gmail.com>
2024-07-07 12:38:21 +0300

    Add simple Variable use example

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
+
+# Variables
+
+string characterName = "John";
+int characterAge = 35;
+
+Console.WriteLine("There once was a man named " + characterName);
+Console.WriteLine("He was " + characterAge + " years old");
+
+Console.ReadLine();
