11572296451c3700470bb38f33c29f7be47ecbdc
Antti Juotasniemi <anttijuo@gmail.com>
2024-07-09 18:09:13 +0300

    Add Data Types section

--- Program.cs
+++ Program.cs
@@ -1,11 +1,23 @@
 ﻿Console.WriteLine("Hello, World!");
 
-# Variables
+// Variables
 
 string characterName = "John";
 int characterAge = 35;
 
 Console.WriteLine("There once was a man named " + characterName);
 Console.WriteLine("He was " + characterAge + " years old");
+
+Console.ReadLine();
+
+// Data Types
+
+char grade = 'A';
+string phrase = "Test Program";
+int age = 30;
+float small = 3.3;
+double big = 3.0003;
+decimal verybig = 3.00000003;
+bool amAlive = true;
 
 Console.ReadLine();
